5dcbddc566e59da3b5f8e84a9df72e272b350d1a
Amo Moloko <u14284783@tuks.co.za>
2017-02-12 14:25:14 +0200

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,37 @@
+﻿var TIMEOUT = 6000;
+
+var interval = setInterval(handleNext, TIMEOUT);
+
+function handleNext() {
+
+    var $radios = $('input[class*="slide-radio"]');
+    var $activeRadio = $('input[class*="slide-radio"]:checked');
+
+    var currentIndex = $activeRadio.index();
+    var radiosLength = $radios.length;
+
+    $radios
+      .attr('checked', false);
+
+    if (currentIndex >= radiosLength - 1) {
+
+        $radios
+          .first()
+          .attr('checked', true);
+
+    } else {
+
+        $activeRadio
+          .next('input[class*="slide-radio"]')
+          .attr('checked', true);
+
+    }
+
+}
+
+
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
